ab6aa77ae471c5c4deabac5120f28606fec2919c
Bram Dufour <britaj@bancolombia.com.co>
2020-02-20 01:41:34 -0500

    add Linux ami data, orderer EIP and orderer server instance

--- modules/orderer/main.tf
+++ modules/orderer/main.tf
@@ -1,1 +1,27 @@
+provider "aws" {
+}
+
+data "aws_ami" "linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*"]
+  }
+}
+
+resource "aws_instance" "server" {
+  ami           = data.aws_ami.linux.id
+  instance_type = var.server_instance_type
+
+  // Add tags
+}
+
+resource "aws_eip" "public_ip" {
+  instance = aws_instance.server.id
+
+  // Add tags
+  // NodeType tag
+}
 
--- modules/orderer/variables.tf
+++ modules/orderer/variables.tf
@@ -1,1 +1,15 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# REQUIRED PARAMETERS
+# You must provide a value for each of these parameters.
+# ---------------------------------------------------------------------------------------------------------------------
 
+
+# ---------------------------------------------------------------------------------------------------------------------
+# OPTIONAL PARAMETERS
+# These parameters have reasonable defaults.
+# ---------------------------------------------------------------------------------------------------------------------
+variable "server_instance_type" {
+  description = "The type of EC2 instance to run for the server of the orderer"
+  type        = string
+  default     = "t2.micro"
+}
