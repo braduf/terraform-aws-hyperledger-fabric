82b3cfba3f336d82ed3b5b89b570f8c52d8eb92a
Bram Dufour <bram.dufour8@gmail.com>
2020-04-03 00:00:54 -0500

    add security group and rules for orderer and operations server inbounds

--- modules/orderer/main.tf
+++ modules/orderer/main.tf
@@ -2,6 +2,10 @@ provider "aws" {
   version = "~> 2.0"
 }
 
+locals {
+  tcp_protocol = "tcp"
+}
+
 data "aws_ami" "linux" {
   most_recent = true
   owners      = ["amazon"]
@@ -15,11 +19,36 @@ data "aws_ami" "linux" {
 resource "aws_instance" "server" {
   ami           = data.aws_ami.linux.id
   instance_type = var.server_instance_type
+  security_groups = [aws_security_group.instance.id]
 
   tags = {
     Name        = ""
     Environment = var.environment
   }
+}
+
+resource "aws_security_group" "instance" {
+  name = "${var.server_name}-instance"
+}
+
+resource "aws_security_group_rule" "allow_orderer_inbound" {
+  type = "ingress"
+  security_group_id = aws_security_group.instance.id
+
+  from_port = var.listen_port
+  to_port = var.listen_port
+  protocol = local.tcp_protocol
+  cidr_blocks = var.listen_ingress_cidr_blocks
+}
+
+resource "aws_security_group_rule" "allow_operations_inbound" {
+  type = "ingress"
+  security_group_id = aws_security_group.instance.id
+
+  from_port = var.operations_port
+  to_port = var.operations_port
+  protocol = local.tcp_protocol
+  cidr_blocks = var.operations_ingress_cidr_blocks
 }
 
 resource "aws_eip" "public_ip" {
--- modules/orderer/variables.tf
+++ modules/orderer/variables.tf
@@ -8,6 +8,12 @@
 # OPTIONAL PARAMETERS
 # These parameters have reasonable defaults.
 # ---------------------------------------------------------------------------------------------------------------------
+variable "server_name" {
+  description = "A name for the EC2 instance to run for the server of the orderer"
+  type        = string
+  default     = "orderer"
+}
+
 variable "server_instance_type" {
   description = "The type of EC2 instance to run for the server of the orderer"
   type        = string
@@ -18,4 +24,28 @@ variable "environment" {
   description = "The environment the server of the orderer"
   type        = string
   default     = "dev"
+}
+
+variable "listen_port" {
+  description = "The port the orderer will listen on"
+  type = string
+  default = "7050"
+}
+
+variable "listen_ingress_cidr_blocks" {
+  description = "The ip addresses allowed to contact the orderer"
+  type = list(string)
+  default = ["0.0.0.0/0"]
+}
+
+variable "operations_port" {
+  description = "The port for the operations server"
+  type = string
+  default = "8443"
+}
+
+variable "operations_ingress_cidr_blocks" {
+  description = "The ip addresses allowed to contact the orderer's operations service"
+  type = list(string)
+  default = ["0.0.0.0/0"]
 }
